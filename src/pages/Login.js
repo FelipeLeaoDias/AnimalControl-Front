5a6b5731601b5f7c70018df7c7e126416d919cfc
Felipe Brasileiro <fbrasileiro-git@0xfb.xyz>
2024-11-23 00:58:26 -0300

    removido bypass de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -58,12 +58,6 @@ export default class Login extends Component {
           </TouchableOpacity>
         </View>
 
-        <View style={styles.forms}>
-        <TouchableOpacity style={styles.registerContainer} onPress={() => navigation.navigate('Fazendas')}>
-          <Text style={styles.registerText}>SemLogin</Text>
-        </TouchableOpacity>
-        </View>
-
         <Text>Não possui conta?</Text>
         <TouchableOpacity style={styles.registerContainer} onPress={() => navigation.navigate('Cadastro')}>
           <Text style={styles.registerText}>Cadastre-se!</Text>
